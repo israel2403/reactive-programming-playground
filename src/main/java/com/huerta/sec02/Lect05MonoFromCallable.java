dee34f697e3dff362e81f2e28a628b9856ccce97
Israel Huerta Fer <israelhf24@gmail.com>
2025-11-10 22:00:22 -0600

    Mono - From Callable

--- src/main/java/com/huerta/sec02/Lect05MonoFromCallable.java
+++ src/main/java/com/huerta/sec02/Lect05MonoFromCallable.java
@@ -9,17 +9,17 @@ import com.huerta.common.Util;
 
 import reactor.core.publisher.Mono;
 
-public class Lect05MonoFromSupplier {
-  private static final Logger log = LoggerFactory.getLogger(Lect05MonoFromCallable.class);
+public class Lect05MonoFromCallable {
+  private static final Logger log = LoggerFactory.getLogger(Lect05MonoFromSupplier.class);
 
   public static void main(String[] args) {
 
     var list = List.of(1, 2, 3);
-    Mono.fromSupplier(() -> sum(list))
+    Mono.fromCallable(() -> sum(list))
         .subscribe(Util.subscriber());
   }
 
-  public static int sum(final List<Integer> list) {
+  public static int sum(final List<Integer> list) throws Exception {
     log.info("finding the sum of {}", list);
     return list.stream().mapToInt(a -> a).sum();
   }
